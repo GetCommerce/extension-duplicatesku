3ca7afc90b09ac10596f38a984a8d345a608af23
getsquare <online@getsquare.co.uk>
2014-07-22 09:52:02 +0100

    Update Observer.php

--- app/local/Getsquare/Duplicatesku/Model/Observer.php
+++ app/local/Getsquare/Duplicatesku/Model/Observer.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ *
+ *
+ * @author  Ross Knight (getsquare.co.uk)
+ * @copyright  Copyright (c) 2014 GetSquare
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+ 
 class Getsquare_Duplicatesku_Model_Observer
 {
     /**
